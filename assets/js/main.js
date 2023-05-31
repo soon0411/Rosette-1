f6c774ac5f944f02ded225232892b4526a495771
jungsoonhwa <soonyusa04@gmail.com>
2023-05-31 16:53:23 +0900

    이미지밀 메인슬라이드적용

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+var swiper = new swiper(".content",{
+    slidesPerView: 3,
+    spaceBetween: 20,
+    grabCursor: "true",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      950: {
+        slidesPerView: 3,
+      },
+    },
+    autoplay: {
+      delay: 1000,
+    },
+    // Infinite Loop Added
+    loop: true,
+  });
